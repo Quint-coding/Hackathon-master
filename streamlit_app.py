84689320a8dece54869fa1329532e67faa0bf65b
Quint-coding <kloq996@gmail.com>
2025-03-24 14:10:44 +0100

    andere path bepaling

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,13 @@ elif page == "🔊 pagina 1":
 
     # df['path'] = df['Latitude'].astype('string') + ", " + df['Longitude'].astype('string')
 
-    df['path'] = df.apply(lambda row: [[row['Longitude'], row['Latitude']]], axis=1)
+    # df['path'] = df.apply(lambda row: [[row['Longitude'], row['Latitude']]], axis=1)
+
+    # Group data into paths (TripsLayer expects an array of coordinate lists)
+    df_grouped = df.groupby(lambda x: 0)[['Longitude', 'Latitude']].apply(lambda x: x.values.tolist()).reset_index(name="path")
+
+    # Add timestamps for animation
+    df_grouped['timestamp'] = [df['timestamp'].tolist()]
 
     layer = pdk.Layer(
         "TripsLayer",
