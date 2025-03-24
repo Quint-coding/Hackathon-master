117d7993d519a7c04c63d6a7844a078b18aab79a
Quint-coding <kloq996@gmail.com>
2025-03-24 11:24:16 +0100

    opmaak en pagina's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,53 @@
 import streamlit as st
 
-st.title('🎈 Geluid overlast')
 
-st.write('Team 8'
-'- Burhan Canbaz'
-'- Jorik Stavenuiter'
-'- Tammo van Leeuwen'
-'- Quint Klaassen')
+
+# Set page configuration
+st.set_page_config(page_title="Schiphol geluidoverlast Dashboard", page_icon="🔊", layout="wide")
+
+# Custom CSS to style the sidebar (dark theme)
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebar"] {
+            background-color: #1E1E1E !important;  /* Dark sidebar */
+        }
+        [data-testid="stSidebar"] h1, 
+        [data-testid="stSidebar"] h2, 
+        [data-testid="stSidebar"] h3, 
+        [data-testid="stSidebar"] h4, 
+        [data-testid="stSidebar"] h5, 
+        [data-testid="stSidebar"] h6, 
+        [data-testid="stSidebar"] label, 
+        [data-testid="stSidebar"] p {
+            color: white !important;  /* White text */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Sidebar Navigation
+st.sidebar.title("📍 Navigatie")
+page = st.sidebar.radio("Ga naar", ["🔊 Home", 
+                                    "🔊 pagina 1", 
+                                    "🔊 pagina 2", 
+                                    "🔊 pagina 3"])
+
+# Home Page
+if page == "🔊 Home":
+    st.title("Geluid overlast")
+    st.subheader("Welkom bij ons schiphol dashboard over geluid overlast")
+
+    st.write("""
+    Dit dashboard geeft inzicht in de overlast veroorzaakt door vliegtuigen van en naar schiphol.
+    Gebruik de navigatie aan de linkerzijde om naar de visualisaties te gaan.
+    
+    
+
+    Team 8:
+    - Tammo van Leeuwen, 
+    - Jorik Stavenuiter, 
+    - Burhan Canbaz, 
+    - Quint Klaassen
+    """)
