9f19076e67aa65d5cb687fc0f9c678cf946bbfc6
Quint-coding <kloq996@gmail.com>
2025-03-24 14:03:45 +0100

    pydeck chart 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
-
+import pandas as pd
+import numpy as np
+import pydeck as pdk
 
 # Set page configuration
 st.set_page_config(page_title="Schiphol geluidoverlast Dashboard", page_icon="🔊", layout="wide")
@@ -57,17 +58,45 @@ if page == "🔊 Home":
     - Burhan Canbaz, 
     - Quint Klaassen
     """)
-elif page == "🔊 Home":
-    st.title("Geluid overlast")
-    st.subheader("Welkom bij ons schiphol dashboard over geluid overlast")
-
-    st.write("""hii""")
 
 elif page == "🔊 pagina 1":
     st.title("Geluid overlast")
     st.subheader("Welkom bij ons schiphol dashboard over geluid overlast")
 
     st.write("""hello""")
+
+    df = pd.read_csv('timestamp vlucht data.csv')
+
+    # df["coordinates"] = df["waypoints"].apply(lambda f: [item["coordinates"] for item in f])
+    # df["timestamps"] = df["waypoints"].apply(lambda f: [item["timestamp"] - 1554772579000 for item in f])
+
+    # df.drop(["waypoints"], axis=1, inplace=True)
+
+    df['path'] = df['Latitude'].astype('string') + ", " + df['Longitude'].astype('string')
+
+    layer = pdk.Layer(
+        "TripsLayer",
+        df,
+        get_path="path",
+        get_timestamps="timestamp",
+        get_color=[253, 128, 93],
+        opacity=0.8,
+        width_min_pixels=5,
+        rounded=True,
+        trail_length=600,
+        current_time=500,
+    )
+
+    view_state = pdk.ViewState(latitude=37.7749295, 
+                               longitude=-122.4194155, 
+                               zoom=11, 
+                               bearing=0, 
+                               pitch=45)
+
+    deck = pdk.Deck(layers=[layer], initial_view_state=view_state)
+
+    # Render the map in Streamlit
+    st.pydeck_chart(deck)
 
 
 elif page == "🔊 pagina 2":
