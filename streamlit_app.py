5f268589d18f3872ba783ef6e520836535ae3e63
Quint-coding <kloq996@gmail.com>
2025-03-25 11:58:49 +0100

    grotere cirkel

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ elif page == "🔊 pagina 1":
         "ScatterplotLayer",
         data=df,
         get_position=["Longitude", "Latitude"],
-        get_radius=['Noise_Level'],
+        get_radius=(['Noise_Level'] * 5),
         get_fill_color="color",
         pickable=True,
         opacity=0.3,
