d0c6661e0ec6b08f52f05882d59bee9133f42fc0
Quint-coding <kloq996@gmail.com>
2025-03-25 12:56:18 +0100

    pathype

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,9 +130,9 @@ elif page == "🔊 Geoplot geluidoverlast":
                     get_path="path",
                     get_width=4,
                     get_color=[100, 100, 255],
-                    alpha= 0.8,
-                    width_min_pixels=2,
+                    # width_min_pixels=2,
                     pickable=True, # pickable toegevoegd.
+                    __annotations__pathType='loop'
                 )
             )
     
