1d03ac896ac3a11f34d807f55c807dfe9a5168fe
Quint-coding <kloq996@gmail.com>
2025-03-25 14:01:56 +0100

    40 vluchten dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,7 +177,7 @@ elif page == "🔊 Geoplot geluidoverlast":
     # # Noise aan toevoegen
     # Maak een pydeck Deck (kaart)
     deck = pdk.Deck(
-    layers=[radius_layer] + route_layers,
+    layers=route_layers + [radius_layer],
     initial_view_state=initial_view_state,
     map_style="mapbox://styles/mapbox/streets-v11",
     tooltip={
