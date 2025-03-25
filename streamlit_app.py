fb1ca84cc210cc988a22ec4e84ba869c36876558
Quint-coding <kloq996@gmail.com>
2025-03-25 14:52:30 +0100

    speed_mph

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ elif page == "🔊 Geoplot geluidoverlast":
                 pdk.Layer(
                     "PathLayer",
                     # data=[{"path": route_coordinates, "FlightNumber": flight_number}],  # FlightNumber toegevoegd
-                    data=[{"path": route_coordinates, "FlightNumber": flight_number, "Course": flight_df['Course'].iloc[0], "Speed": flight_df['Speed'].iloc[0]}],
+                    data=[{"path": route_coordinates, "FlightNumber": flight_number, "Course": flight_df['Course'].iloc[0], "Speed": flight_df['Speed_mph'].iloc[0]}],
                     get_path="path",
                     get_width=4,
                     get_color=[100, 100, 255],
@@ -179,7 +179,7 @@ elif page == "🔊 Geoplot geluidoverlast":
     initial_view_state=initial_view_state,
     map_style="mapbox://styles/mapbox/streets-v11",
     tooltip={
-        "html": "<b>Vlucht ID:</b> {FlightNumber}<br/><b>Course:</b> {Course}<br/><b>Speed:</b> {Speed}",
+        "html": "<b>Vlucht ID:</b> {FlightNumber}<br/><b>Course:</b> {Course}<br/><b>Speed (mph):</b> {Speed}",
         "style": {
             "backgroundColor": "white",
             "color": "black",
