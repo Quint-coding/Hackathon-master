57d42887c5cf63b3e2a2e689b94bfd2afdf163aa
Quint-coding <kloq996@gmail.com>
2025-03-26 14:52:48 +0100

    noise level expanded

--- streamlit_app.py
+++ streamlit_app.py
@@ -336,15 +336,15 @@ elif page == "🔊 pagina 3":
             "z-index": "10000"
         }
     }
-    
-    df_to_visualize['Noise_Level'] = df_to_visualize['Noise_Level'] * 10
+
+    df_to_visualize['Noise_Level_expanded'] = df_to_visualize['Noise_Level'] * 10
 
     # Geluidsimpact toevoegen als cirkels rond elke locatie (gebaseerd op df_to_visualize)
     radius_layer = pdk.Layer(
         "ScatterplotLayer",
         data=df_to_visualize,
         get_position=["Longitude", "Latitude"],
-        get_radius='Noise_Level',
+        get_radius='Noise_Level_expanded',
         get_fill_color="color",
         pickable=True,
         opacity=0.3
