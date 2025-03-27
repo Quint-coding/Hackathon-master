191415452ae79b863acb145a97b58b9602c1e5a6
Quint-coding <kloq996@gmail.com>
2025-03-27 12:49:36 +0100

    figs

--- streamlit_app.py
+++ streamlit_app.py
@@ -253,17 +253,27 @@ elif page == "🔊 pagina 3":
 
     plane_specs = pd.read_csv('plane_specs_zonder_fouten.csv')
 
-    fig1 = px.bar(plane_specs, 'seats', 'range_km', color='type', title='Actieradius per passagierscapaciteit')
+    fig1 = px.bar(plane_specs, 'type', 'seats', color='type', title='Passagierscapaciteit per vliegtuigmodel')
 
-    fig2 = px.bar(plane_specs, 'max_takeoff_weight_t', 'range_km', color='type', title='Actieradius per startgewicht (\'massa rijklaar\' in autotermen)')
+    fig2 = px.bar(plane_specs, 'max_takeoff_weight_t', 'type', color='type', title='Startgewicht (\'massa rijklaar\' in autotermen) per vliegtuigmodel')
 
-    fig3 = px.bar(plane_specs, 'empty_weight_t', 'range_km', color='type', title='Actieradius per leeggewicht (\'massa ledig voertuig\' in autotermen)')
+    fig3 = px.bar(plane_specs, 'type', 'range_km', color='type', title='Actieradius per vliegtuigmodel')
 
-    fig4 = px.bar(plane_specs, 'seats', 'ceiling_m', color='type', title='Dienstplafond per passagierscapaciteit')
+    fig4 = px.bar(plane_specs, 'seats', 'range_km', color='type', title='Actieradius per passagierscapaciteit')
 
-    fig5 = px.bar(plane_specs, 'max_takeoff_weight_t', 'ceiling_m', color='type', title='Dienstplafond per startgewicht (\'massa rijklaar\' in autotermen)')
+    fig5 = px.bar(plane_specs, 'max_takeoff_weight_t', 'range_km', color='type', title='Actieradius per startgewicht (\'massa rijklaar\' in autotermen)')
 
-    fig6 = px.bar(plane_specs, 'empty_weight_t', 'ceiling_m', color='type', title='Dienstplafond per leeggewicht (\'massa ledig voertuig\' in autotermen)')
+    fig6 = px.bar(plane_specs, 'empty_weight_t', 'range_km', color='type', title='Actieradius per leeggewicht (\'massa ledig voertuig\' in autotermen)')
+
+    fig7 = px.bar(plane_specs, 'wingspan_m', 'range_km', color='type', title=('Actieradius per spanwijdte'))
+
+    # fig4 = px.bar(plane_specs, 'seats', 'ceiling_m', color='type', title='Dienstplafond per passagierscapaciteit')
+
+    # fig5 = px.bar(plane_specs, 'max_takeoff_weight_t', 'ceiling_m', color='type', title='Dienstplafond per startgewicht (\'massa rijklaar\' in autotermen)')
+
+    # fig6 = px.bar(plane_specs, 'empty_weight_t', 'ceiling_m', color='type', title='Dienstplafond per leeggewicht (\'massa ledig voertuig\' in autotermen)')
+
+
 
     st.plotly_chart(fig1)
     st.plotly_chart(fig2)
@@ -271,3 +281,4 @@ elif page == "🔊 pagina 3":
     st.plotly_chart(fig4)
     st.plotly_chart(fig5)
     st.plotly_chart(fig6)
+    st.plotly_chart(fig7)
